d02e04c3b2d70f8a2649922815b0768bc93ab9b0
mhahsler <michael@hahsler.net>
2025-09-22 11:28:29 -0500

    preserve book PDF.

--- book_src/render_html.sh
+++ book_src/render_html.sh
@@ -7,6 +7,11 @@ pkgs_install <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
 if(length(pkgs_install)) install.packages(pkgs_install)
 
 
+# remove old version but save the book file
+file.rename("../book/R-Companion-Data-Mining.pdf", "./R-Companion-Data-Mining.pdf")
+unlink("../book", recursive=TRUE)
+file.rename("./R-Companion-Data-Mining.pdf", "../book/R-Companion-Data-Mining.pdf")
+
 if (length(params) == 0) {
 	bookdown::render_book("index.Rmd", output_format = "bookdown::bs4_book")
 } else { 
