1ae7f3ec9664744b4813222401364bc8bdededeb
mhahsler <michael@hahsler.net>
2025-10-15 16:24:10 -0500

    copy PDF after creating the html pages.

--- book_src/render_html.sh
+++ book_src/render_html.sh
@@ -6,17 +6,18 @@ pkgs <- "bookdown"
 pkgs_install <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
 if(length(pkgs_install)) install.packages(pkgs_install)
 
-
 # remove old version but save the book file
 file.rename("../book/R-Companion-Data-Mining.pdf", "./R-Companion-Data-Mining.pdf")
 unlink("../book", recursive=TRUE)
-file.rename("./R-Companion-Data-Mining.pdf", "../book/R-Companion-Data-Mining.pdf")
+
 
 if (length(params) == 0) {
 	bookdown::render_book("index.Rmd", output_format = "bookdown::bs4_book")
 } else { 
 	bookdown::preview_chapter(params, output_format = "bookdown::bs4_book")
 }
+
+file.rename("./R-Companion-Data-Mining.pdf", "../book/R-Companion-Data-Mining.pdf")
 
 warnings()
 
